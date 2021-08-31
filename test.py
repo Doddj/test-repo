7fd1a1a178f4395591c15199d295f788e3a123e6
Daniel Jing <jing.daniel.a@gmail.com>
2021-08-31 03:51:20 -0500

    Code spec demo

---
+++ test.py
@@ -0,0 +1,15 @@
+# Important class
+class ImportantClass:
+       
+    def __init__(self, var):
+           
+        # Instance variable
+        self.var = var
+   
+    # Important function 1
+    def importantFunction(self, old_var, new_var):
+        return old_var + new_var
+
+    # Make users happy
+    def makeUsersHappy(self, users):
+        print("Success!")
